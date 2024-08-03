9af5a6bbd9568e22faeac052569a51a6eebc1f72
Muhammad Hatta <muhammadhatta834@gmail.com>
2024-08-03 08:38:34 +0700

    update

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -4,52 +4,76 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const projects = [
   {
-    title: 'Project One',
+    title: '#0001',
     description: 'Description for project one.',
-    link: 'https://hatta-dinata.pages.dev/',
-    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://hatta-dinata.pages.dev/&dimension=1280x870`
+    link: 'https://e3613262.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://e3613262.digitalkreatif.pages.dev/&dimension=1280x870`
   },
   {
-    title: 'Project Two',
+    title: '#0002',
     description: 'Description for project two.',
-    link: 'https://contoh-digif.pages.dev/',
-    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://contoh-digif.pages.dev/&dimension=1280x870`
+    link: 'https://d2d2c8ad.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://d2d2c8ad.digitalkreatif.pages.dev/&dimension=1280x870`
   },
   {
-    title: 'Project One',
+    title: '#0003',
     description: 'Description for project one.',
-    link: 'https://hatta-dinata.pages.dev/',
-    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://hatta-dinata.pages.dev/&dimension=1280x870`
+    link: 'https://c30d31f8.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://c30d31f8.digitalkreatif.pages.dev/&dimension=1280x870`
   },
   {
-    title: 'Project Two',
+    title: '#0004',
     description: 'Description for project two.',
-    link: 'https://contoh-digif.pages.dev/',
-    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://contoh-digif.pages.dev/&dimension=1280x870`
+    link: 'https://0f69e2d1.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://0f69e2d1.digitalkreatif.pages.dev/&dimension=1280x870`
   },
   {
-    title: 'Project One',
+    title: '#0005',
     description: 'Description for project one.',
-    link: 'https://hatta-dinata.pages.dev/',
-    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://hatta-dinata.pages.dev/&dimension=1280x870`
+    link: 'https://25ef1f3a.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://25ef1f3a.digitalkreatif.pages.dev/&dimension=1280x870`
   },
   {
-    title: 'Project Two',
+    title: '#0006',
     description: 'Description for project two.',
-    link: 'https://contoh-digif.pages.dev/',
-    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://contoh-digif.pages.dev/&dimension=1280x870`
+    link: 'https://51bffcae.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://51bffcae.digitalkreatif.pages.dev/&dimension=1280x870`
   },
   {
-    title: 'Project One',
+    title: '#0007',
     description: 'Description for project one.',
-    link: 'https://hatta-dinata.pages.dev/',
-    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://hatta-dinata.pages.dev/&dimension=1280x870`
+    link: 'https://a2aca4e0.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://a2aca4e0.digitalkreatif.pages.dev/&dimension=1280x870`
   },
   {
-    title: 'Project Two',
+    title: '#0008',
     description: 'Description for project two.',
-    link: 'https://contoh-digif.pages.dev/',
-    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://contoh-digif.pages.dev/&dimension=1280x870`
+    link: 'https://8128d611.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://8128d611.digitalkreatif.pages.dev/&dimension=1280x870`
+  },
+  {
+    title: '#0009',
+    description: 'Description for project two.',
+    link: 'https://cd551b02.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://cd551b02.digitalkreatif.pages.dev/&dimension=1280x870`
+  },
+  {
+    title: '#0010',
+    description: 'Description for project two.',
+    link: 'https://a2384a8f.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://a2384a8f.digitalkreatif.pages.dev/&dimension=1280x870`
+  },
+  {
+    title: '#0011',
+    description: 'Description for project two.',
+    link: 'https://d0d7d699.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://d0d7d699.digitalkreatif.pages.dev/&dimension=1280x870`
+  },
+  {
+    title: '#0012',
+    description: 'Description for project two.',
+    link: 'https://9a73dcd9.digitalkreatif.pages.dev/',
+    image: `https://api.screenshotmachine.com?key=${API_KEY}&url=https://9a73dcd9.digitalkreatif.pages.dev/&dimension=1280x870`
   },
   // Add more projects here
 ];
