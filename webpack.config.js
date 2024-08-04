fb0da0c8dcecedf1c5c6404c71acd549c30630fb
Muhammad Hatta <muhammadhatta834@gmail.com>
2024-08-04 10:56:33 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,18 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'media'
+            }
+          }
+        ]
       }
     ]
   },
